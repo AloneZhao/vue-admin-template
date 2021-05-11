82eadcc366248f99f4b771da90490cb922b7a545
Mr_zhao <originaldream_z@163.com>
2021-05-11 14:37:35 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description 固定头部
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description 侧边栏logo的显示与否
+   */
+  sidebarLogo: false
+}
